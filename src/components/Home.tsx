fb919e6e209deaa56d32608b38e97734d630f0c2
raju <raju.vangipurapu@everestdx.com>
2024-12-10 09:37:36 +0530

    upadted code

--- src/components/Home.tsx
+++
@@ -1,4 +0,0 @@
-export const Home = () => {
-  console.log("Home component rendered");
-  return <h1>Welcome Home</h1>;
-};
--- src/components/NoPage.tsx
+++
@@ -1,3 +0,0 @@
-export const NoPage = () => {
-  return <h1>404 Not Found.</h1>;
-};
