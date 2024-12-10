8f487ebc44364717b640aa25c912ca8191d91750
raju <raju.vangipurapu@everestdx.com>
2024-12-10 09:21:05 +0530

    updated code

---
+++ src/app/router.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+const ErrorPage = () => (
+  <div style={{ textAlign: "center", fontSize: "50px" }}>
+    404 Page Not Found.
+  </div>
+);
+
+export const createAppRouter = () => {
+  return createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: (
+          <>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "20px",
+              }}
+            >
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/users">Users</Link>
+            </div>
+            <br />
+            <Outlet />
+          </>
+        ),
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <div>Home</div> },
+          { path: "about", element: <div>About</div> },
+          { path: "users", element: <div>Users</div> },
+        ],
+      },
+    ],
+    { basename: "/my-app" }
+  );
+};
+
+export const AppRouter = () => {
+  const router = React.useMemo(() => createAppRouter(), []);
+  return <RouterProvider router={router} />;
+};
--- src/components/Users.tsx
+++
@@ -1,3 +0,0 @@
-export const Users = () => {
-  return <h1>Welcome Users</h1>;
-};
